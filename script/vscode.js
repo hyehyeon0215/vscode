d9dce8c8209e68cd40c4769adde5d19303ee7d37
hyehyeon0215 <qwert0880@naver.com>
2023-09-20 13:13:45 +0900

    메뉴 js 구현

--- script/vscode.js
+++ script/vscode.js
@@ -2,6 +2,13 @@ const header_gnb = document.querySelectorAll('.header-menu > li');
 const bar_icon = document.querySelectorAll('.bar-icon > li');
 const gnb_1depth = document.querySelector('.menu_1depth > li > a ');
 
+const menu_portfolio = document.querySelector('.menu-portfolio');
+const menu_portfolioBtn = menu_portfolio.querySelector('.bracket');
+const menu_portfolioList = menu_portfolio.querySelector('ul');
+
+const menu_Btn = document.querySelector('.menu-btn > .bracket');
+const menu_List = document.querySelector('.menu_2depth');
+
 header_gnb.forEach((el)=>{
     el.addEventListener(("click"),(e)=>{
         e.preventDefault();
@@ -24,4 +31,28 @@ bar_icon.forEach((el)=>{
         })
         el.classList.add('on');
     })
+})
+
+menu_portfolioBtn.addEventListener(("click"), (e)=>{
+    e.preventDefault();
+
+    if(menu_portfolioBtn.classList.contains('off')) {
+        menu_portfolioBtn.classList.remove('off');
+        menu_portfolioList.classList.remove('off');
+    } else {
+        menu_portfolioBtn.classList.add('off');
+        menu_portfolioList.classList.add('off');
+    }
+})
+
+menu_Btn.addEventListener(("click"), (e)=>{
+    e.preventDefault();
+
+    if(menu_Btn.classList.contains('off')) {
+        menu_Btn.classList.remove('off');
+        menu_List.classList.remove('off');
+    } else {
+        menu_Btn.classList.add('off');
+        menu_List.classList.add('off');
+    }
 })
